d7bbae3b87d24d7728c23a2987535457dca9db0a
pxgamer <owzie123@gmail.com>
2017-11-09 17:14:07 +0000

    Added () to initialisation of classes in tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -8,13 +8,13 @@ class MainTest extends TestCase
 {
     public function testCanBeInitialised()
     {
-        $beautifier = new Beautifier;
+        $beautifier = new Beautifier();
         $this->assertInstanceOf(Beautifier::class, $beautifier);
     }
 
     public function testCanFixHTML()
     {
-        $beautifier = new Beautifier;
+        $beautifier = new Beautifier();
         $response = $beautifier->html('<a href="https://github.com/">GitHub</a>');
         $response_object = json_decode($response);
         $this->assertObjectHasAttribute('clean', $response_object);
@@ -22,7 +22,7 @@ class MainTest extends TestCase
 
     public function testCanFixCSS()
     {
-        $beautifier = new Beautifier;
+        $beautifier = new Beautifier();
         $response = $beautifier->css('.test{ color: red } ');
         $response_object = json_decode($response);
         $this->assertObjectHasAttribute('clean', $response_object);
@@ -30,7 +30,7 @@ class MainTest extends TestCase
 
     public function testCanFixJavascript()
     {
-        $beautifier = new Beautifier;
+        $beautifier = new Beautifier();
         $response = $beautifier->js('function test() { var a= 0; console.log(a);}');
         $response_object = json_decode($response);
         $this->assertObjectHasAttribute('clean', $response_object);
